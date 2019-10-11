343a5b8c22ec75249e519ddce8f149131cea1f08
Pascal Andermatt <pascal.andermatt@students.fhnw.ch>
2019-10-11 16:58:16 +0200

    fixed error

--- Calculator/Pascal/calculator.js
+++ Calculator/Pascal/calculator.js
@@ -2,7 +2,12 @@
 const pair = x => y => selector => selector(x)(y);
 const fst = x => y => x;
 const snd = x => y => y;
+
 const pairMap = f => p => pair(f(p(fst)))(f(p(snd)));
+
+// Math-Operations
+const add = n1 => n2 => n1 + n2;
+const multiply = n1 => n2 => n1 * n2;
 
 // Pair-Operation abstraction
 const pairOp = op => p => pair(op(p(fst))(p(snd)));
@@ -12,6 +17,3 @@ const pairAdd = pairOp(add);
 const pairMultiply = pairOp(multiply());
 
 
-// Math-Operations
-const add = n1 => n2 => n1 + n2;
-const multiply = n1 => n2 => n1 * n2;
