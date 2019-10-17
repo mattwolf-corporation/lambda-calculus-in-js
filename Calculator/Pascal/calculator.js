1a0b2bd133ddef85dddc6350ae1e7a8d1a305746
Pascal Andermatt <pascal.andermatt@students.fhnw.ch>
2019-10-17 19:46:14 +0200

    added fibonacci with z combinator

--- Calculator/Pascal/calculator.js
+++ Calculator/Pascal/calculator.js
@@ -1,3 +1,10 @@
+// Math-Operations
+const add = n1 => n2 => n1 + n2;
+const multiply = n1 => n2 => n1 * n2;
+
+const multiplyTwo = multiply(2);
+const addTen = add(10);
+
 // pair construction
 const pair = x => y => selector => selector(x)(y);
 const fst = x => y => x;
@@ -5,15 +12,18 @@ const snd = x => y => y;
 
 const pairMap = f => p => pair(f(p(fst)))(f(p(snd)));
 
-// Math-Operations
-const add = n1 => n2 => n1 + n2;
-const multiply = n1 => n2 => n1 * n2;
 
 // Pair-Operation abstraction
 const pairOp = op => p => pair(op(p(fst))(p(snd)));
 
 // pair addition: addition of the two values from the pair
 const pairAdd = pairOp(add);
+
+// pair multiply: multiply of the two values from the pair
 const pairMultiply = pairOp(multiply());
+
+// Test values for debugging
+const p1 = pair(3)(5);
+const p2 = pair(2)(7);
 
 
