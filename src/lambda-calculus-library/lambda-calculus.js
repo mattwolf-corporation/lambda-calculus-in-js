a6911dc6fc1bc8af04eb2946537dd4d9f9017cbd
benjamin.brodwolf <benjamin.brodwolf@students.fhnw.ch>
2020-01-07 22:51:20 +0100

    JSDoc continue refactoring

--- src/lambda-calculus-library/lambda-calculus.js
+++ src/lambda-calculus-library/lambda-calculus.js
@@ -10,54 +10,61 @@ export {I as id, M, K, KI, C, B, T, V, Blackbird, fst, snd, firstOfTriple, secon
  * @typedef {function} gn
  * @typedef {function} pn
  * @typedef {function} qn
+ * @typedef {function} boolean
  */
 
 /**
  * a -> a ; Identity (id)
  * @param   {a} x
- * @returns {a} {@link x}
+ * @returns {a} the Identity {@link a}
  */
 const I = x => x;
 
 /**
  * a -> b -> a ; Kestrel (Constant)
  * @param {a} x
- * @returns { function(y:{b}): {a} } a function that ignores its argument and returns {@link x}
+ * @returns { function(y:b): function(x:{a}) } a function that ignores its argument and returns {@link a}
  */
 const K = x => y => x;
 
 /**
  * a -> b -> b ; Kite
  * @param {a} x
- * @returns { function(y:{b}): {b} } a function that returns its argument {@link y}
+ * @returns { function(y:{b}): {b} } a function that returns its argument {@link a}
  */
 const KI = x => y => y;
 
 /**
  * fn -> fn( fn ) ; Mockingbird
  * @param {fn} f
- * @returns {  function(f {fn})  }
+ * @returns { function(f {fn}) } a self-application combinator
  */
 const M = f => f(f);
 
 /**
  * fn -> a -> b -> fn( b )( a ) ; Cardinal (flip)
  * @param  {fn} f
- * @returns { function(x:{a}): function(y:{b}): function(fn y:{b} x:{a}  ) }
+ * @returns { function(x:{a}): function(y:{b}): function(fn y:{b} x:{a} ) } The Cardinal, aka flip, takes two-argument function, and produces a function with reversed argument order.
  */
 const C = f => x => y => f(y)(x);
 
 /**
- * fn -> gn -> a -> fn( gn(a) ) ; Bluebird (Function composition)
+ * fn -> gn -> a -> fn( gn( a ) ) ; Bluebird (Function composition)
  * @param {fn} f
- * @returns { function(g:{gn}): function(x:{a}):  function({ fn:{ gn:{a} } } ) }
+ * @returns { function(g:{gn}): function(x:{a}):  function({ fn:{ gn:{a} } } ) } two-fold self-application composition
+ *
+ * @example
+ * B(id)(id)(n7) === n7
+ * B(id)(jsnum)(n7) === 7
+ * B(not)(not)(True) === True
  */
 const B = f => g => x => f(g(x));
 
 /**
  * a -> fn -> fn( a ) ; Thrush (hold an argument)
+ * @function T
  * @param {a} x
- * @returns { function(f:{fn}): function(f x:{a} ) }
+ * @returns { function(f:{fn}): function(f x:{a} ) } self-application with holden argument
  */
 const T = x => f => f(x);
 
@@ -87,7 +94,7 @@ const not = C;
 /**
  * pn -> qn -> pn( qn )(not( qn)) ; Boolean-Equality
  * @param {pn} p
- * @returns { function(q:{qn}): { pn(q:{qn}) (not (q:{qn}) ) } }
+ * @returns { function(q:{qn}):  True/False  }
  */
 const beq = p => q => p(q)(not(q));
 
@@ -99,12 +106,18 @@ const convertToJsBool = b => b(true)(false);
  * @param {*} x:  firstOfPair argument of the pair
  * @returns {function} - returns a function, that takes an argument y
  */
-const pair = x => y => f => f(x)(y);
+const pair = V; //x => y => f => f(x)(y);
 
 /**
  * fst ; Get first value of Pair
+ * @type {K.props|*}
+ *
+ * @return pair first storaged value
+ * @example
+ * par(n1)(n2)(fst) === n1
  */
 const fst = K;
+
 const snd = KI;
 
 /**
@@ -114,9 +127,6 @@ const snd = KI;
  */
 const triple = x => y => z => f => f(x)(y)(z);
 
-
-
-
 // triple getter
 const firstOfTriple = x => y => z => x;
 const secondOfTriple = x => y => z => y;
@@ -124,6 +134,12 @@ const thirdOfTriple = x => y => z => z;
 
 const pairMap = f => p => pair(f(p(fst)))(f(p(snd)));
 
+
+/**
+ *
+ * @param p
+ * @return {string}
+ */
 const showPair = p => `${p(fst)} | ${p(snd)}`;
 
 
