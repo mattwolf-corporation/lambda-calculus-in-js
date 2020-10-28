20a9b34c7d2104e8867c622351be5f72a354d122
benjamin.brodwolf <benjamin.brodwolf@students.fhnw.ch>
2020-10-28 14:06:39 +0100

    lambdafizierter Observable v.3 - removeListener - logListener
    TODO: getValue not possible because byValue and out of the scope

--- src/Beni/observable/observable.js
+++ src/Beni/observable/observable.js
@@ -1,3 +1,47 @@
+
+const id = x => x;
+
+const addToList = listener => callback => listener.push(callback)
+const notify = listener => val => listener.forEach(l => l(val))
+
+
+const pipe = (...fns) => x => fns.reduce((v, fn) => fn(v), x)
+
+const execute = (...fns) => returnValue => {
+    fns.reduce((_, fn) => fn); // use immutable Stack
+    return returnValue
+}
+
+const removeFromList = array => index => {
+    if(index >= 0) array.splice(index, 1)
+}
+
+const ObsObject = listeners => callback => val => obsFn => obsFn(listeners)(callback)(val)
+
+const InitObservable = initVal => ObsObject([])(x => x)(initVal)
+
+const getValue = listeners => callback => val => val
+
+const addListener = listeners => callback => val => newCallback => execute(
+    listeners.push(newCallback),
+    newCallback(val)
+)(ObsObject(listeners)(newCallback)(val))
+
+const setValue = listeners => callback => val => newVal => execute(
+    listeners.forEach(callback => callback(newVal))
+)()
+
+const removeListener = listeners => callback => val => index => execute(
+    removeFromList(listeners)(index), //listeners.splice(index, 1),  //
+    logListener(listeners)(callback)(val)
+)()
+
+const logListener = listeners => callback => val => execute(
+    console.log("Index\t:  Listener"),
+    listeners.forEach((l, i) => console.log(i + "\t\t:  " + l))
+)()
+
+
 const Observable = value => {
     const listeners = [];
     return {
@@ -14,54 +58,6 @@ const Observable = value => {
         }
     }
 };
-const id = x => x;
-const Blackbird = f => g => x => y => f(g(x)(y));
-
-const addToList = listener => callback => listener.push(callback)
-const notify = listener => val => listener.forEach(l => l(val))
-
-const ObsObject = listeners => callback => val => obsFn => obsFn(listeners)(callback)(val)
-
-
-// const addListener = listeners => callback => val =>
-//                                         newCallback =>
-//                                             (x => addToList(listeners)(newCallback)(id))
-//                                             (x => notify(listeners)(val)(id))
-
-const pipe = (...fns) => x => fns.reduce((v, fn) => fn(v), x)
-
-const execute = (...fns) => returnValue => {
-    fns.reduce((_, fn) => fn);
-    return returnValue
-}
-
-const InitObservable = initVal => ObsObject([])(x => x)(initVal)
-
-const getValue = listeners => callback => val => val
-
-const addListener = listeners => callback => val => newCallback => execute(
-    listeners.push(newCallback),
-    newCallback(val)
-)(ObsObject(listeners)(newCallback)(val))
-
-const setValue = listeners => callback => val => newVal => execute(
-    listeners.forEach(callback => callback(newVal))
-)(ObsObject(listeners)(callback)(newVal))
-
-
-// const addListener = listeners => callback => val => newCallback => { // lambdafizieren mit compose
-//     listeners.push(newCallback)
-//     newCallback(val)
-//     return ObsObject(listeners)(newCallback)(val)
-// }
-
-// const setValue = listeners => callback => val => newVal => {
-//     if (val === newVal) return;
-//     listeners.forEach(callback => callback(newVal));
-//     return ObsObject(listeners)(callback)(newVal)
-// }
-
-
 
 const ObservableList = newList => {
     const list = [...newList]
