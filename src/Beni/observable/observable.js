1f94fe6d403c9799bff42de950f5dc3770a44f39
benjamin.brodolf@students.fhnw.ch <skaten88>
2020-11-05 00:25:04 +0100

    colorpicker improvement

--- src/Beni/observable/observable.js
+++ src/Beni/observable/observable.js
@@ -1,64 +1,45 @@
+// const addToList = listener => callback => listener.push(callback)
+// const notify = listener => val => listener.forEach(l => l(val))
+
+// const pipe = (...fns) => x => fns.reduce((v, fn) => fn(v), x)
+
+// const execute = (...fns) => returnValue => {
+//     fns.reduce((_, fn) => fn); // use immutable Stack
+//     return returnValue
+// }
 
 
+const ObsObject = listeners => obsFn => obsFn(listeners)
+const InitObservable = ObsObject(emptyStack)
 
-const addToList = listener => callback => listener.push(callback)
-const notify = listener => val => listener.forEach(l => l(val))
 
-const pipe = (...fns) => x => fns.reduce((v, fn) => fn(v), x)
+// Obseverable Functions obsFN
+const addListener = listeners => newCallback =>
+    (ObsObject(push(listeners)(newCallback)))
 
-const execute = (...fns) => returnValue => {
-    fns.reduce((_, fn) => fn); // use immutable Stack
-    return returnValue
+const setValue = listeners => newVal =>
+    forEach(listeners)((callback, index) => callback(newVal))
+
+const removeListener = listeners => index =>
+    (ObsObject(reduce(listeners)(pair( rf(index)  )( emptyStack ))))
+
+const rf = index => (acc, curr) => {
+    if(jsnum(size(acc)) === index){
+        return acc;
+    }
+    return push(acc)(curr)
 }
 
-const logListener = s => {
 
+const logListener = s => {
     const logIteration = (acc, curr) => {
         const index = acc + 1;
         const val = typeof(curr) === 'object' ? JSON.stringify(curr) : curr;
         console.log('element at: ' + index + ': ' + val);
         return index;
     };
-
     reduce(s)(pair(logIteration)(0));
 };
-
-const removeFromList = array => index => {
-    if (index >= 0) array.splice(index, 1)
-}
-
-const ObsObject = listeners => obsFn => obsFn(listeners)
-
-const InitObservable = ObsObject(emptyStack)
-
-const getValue = listeners => val => val
-
-// Obseverable Functions obsFN
-const addListener = listeners => newCallback =>
-    (ObsObject(push(listeners)(newCallback)))
-
-const rf = index => (acc, curr) => {
-    // console.log(jsnum(size(acc)))
-
-    if(jsnum(size(acc)) === index){
-        return acc;
-    }
-    // logListener(acc)
-    return push(acc)(curr)
-}
-
-const removeListener = listeners => index =>
-    (ObsObject(reduce(listeners)(pair( rf(index)  )( emptyStack ))))
-
-const setValue = listeners => newVal =>
-    forEach(listeners)((callback, index) => callback(newVal))
-
-
-
-
-    //     console.log("Index\t:  Listener"),
-    // forEach(listeners)((l, i) => console.log(i + "\t\t:  " + l))
-
 
 const Observable = value => {
     const listeners = [];
