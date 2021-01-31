1816dfe9159a39eb597270e64f1bdae6d24657e0
Pascal Andermatt <pascal.andermatt@students.fhnw.ch>
2021-01-31 19:14:39 +0100

    refactoring playground stuff

--- src/Pascal/playground.js
+++ src/Pascal/playground.js
@@ -969,9 +969,33 @@ const fromNullable = x =>
 const findColor = name =>
     fromNullable({red: '#ff4444', blue: '#3b5998', yellow: '#fff68f'}[name])
 
+
+// const Right = x =>
+//     ({
+//         map: f => Right(f(x)),
+//         fold: (f, g) => g(x),
+//         inspect: () => `Right(${x})`
+//     })
+//
+// const Left = x =>
+//     ({
+//         map: f => Left(x),
+//         fold: (f, g) => f(x),
+//         inspect: () => `Left(${x})`
+//     })
+//
+// const res = findColor('blue')
+//     .map(c => c.slice(1))
+//     .map(c => c.toUpperCase())
+//     .fold(e => 'no color', x => x)
+
 const findCol = c =>
     Box2(findColor(c))
     (mapf3)(c => c.slice(1))
+    (mapf3)(c => {
+        console.log("debug: " + c)
+        return c;
+    })
     (mapf3)(c => c.toUpperCase())
     (fold3)(() => 'no color')(id)
 
