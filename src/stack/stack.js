3a9f62d471f8ee6bb8eade9c92c456849428d129
Pascal Andermatt <pascal.andermatt@students.fhnw.ch>
2021-03-12 13:36:14 +0100

    refactoring

--- src/stack/stack.js
+++ src/stack/stack.js
@@ -480,7 +480,7 @@ const convertArrayToStack = array => array.reduce((acc, curr) => push(acc)(curr)
  *
  * convertArrayToStack( stackWithValues ) === [1,2,3]
  */
-const convertElementsToStack = (...elements) => convertStackToArray(elements);
+const convertElementsToStack = (...elements) => convertArrayToStack(elements);
 
 /**
  *  A function that accepts a stack. The function returns the reversed stack.
