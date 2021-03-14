99b169b46e66d5ca410f4ab7ac08d1090182c4cb
Benjamin Brodwolf <benjamin.brodolf@students.fhnw.ch>
2021-03-14 12:21:11 +0100

    refactor

--- src/observable/observableExamples/observableHttpGetJokeExample/observableHttpGetJokeExample.js
+++ src/observable/observableExamples/observableHttpGetJokeExample/observableHttpGetJokeExample.js
@@ -7,14 +7,15 @@ import {convertElementsToStack, convertStackToArray, forEach, logStackToConsole}
 import {convertListMapToArray, convertObjToListMap, getElementByKey} from "../../../listMap/listMap.js";
 import {speak} from "../observableUtilities.js";
 
+// Either all the necessary Dom-Element exist and or display all missed Element
 eitherElementsOrErrorsByFunction(eitherDomElement)("jokeHistory", "norrisBtn", "nerdyBtn", "trumpBtn")
 (err => document.body.innerHTML = Box(err)(mapf)(convertStackToArray)(mapf)(s => s.join(", <br>"))(fold)(txt => `<div style="background: orangered"> <br> ${txt}</div>`))
 (result => {
 
-    // receive the elements for use
+    // receive founded the elements
     const [jokeHistory, norrisBtn, nerdyBtn, trumpBtn] = convertListMapToArray(result)
 
-    // create the Listeners (text-to-speech & diplay to view)
+    // create the Listeners (text-to-speech & display to view)
     const listenerSpeak = newListener(nValue => oValue => speak(nValue(snd)));
     const listenerJokeToDom = newListener(nValue => oValue => {
         const template = document.createElement('fieldset');
@@ -30,20 +31,24 @@ eitherElementsOrErrorsByFunction(eitherDomElement)("jokeHistory", "norrisBtn", "
                                     (addListener)( listenerJokeToDom )
 
 
+    // Jokes-API-URLs
     const jokeNorrisUrl = "https://api.chucknorris.io/jokes/random";            // jsonKey: value
     const jokeNerdUrl   = "https://v2.jokeapi.dev/joke/Programming?type=single" // jsonKey: joke
     const trumpTweetUrl = "https://www.tronalddump.io/random/quote";            // jsonKey: value
 
 
+    // Constructor for a Joke-Object
     const jokeCtor = name => btn => url => jsonKey => convertObjToListMap({name, btn, url, jsonKey});
 
+    // creat Joke-Object
     const norrisJoke = jokeCtor("Chuck Norris - Joke ")(norrisBtn)(jokeNorrisUrl)("value");
     const nerdJoke   = jokeCtor("Nerd - Joke ")(nerdyBtn)(jokeNerdUrl)("joke");
     const trumpTweet = jokeCtor("Trump Tweet")(trumpBtn)(trumpTweetUrl)("value");
 
+    // combine the Joke-Objects into a stack
     const jokes = convertElementsToStack(norrisJoke, nerdJoke, trumpTweet);
 
-// add the Joke-Buttons a on-click event listener for request the Jokes and update the Observable
+    // add the Joke-Buttons a on-click event listener for request the Jokes and update the Observable
     forEach(jokes)((joke, _) =>
         getElementByKey(joke)("btn").onclick = _ =>
             HttpGet( getElementByKey(joke)("url") )(resp =>
