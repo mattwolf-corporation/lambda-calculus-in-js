b07d2e627bb4faa0451c865b4e927d755d29dc36
Benjamin Brodwolf <benjamin.brodolf@students.fhnw.ch>
2021-03-24 23:26:41 +0100

    refactor

--- src/lambda-calculus-library/lambda-calculus.js
+++ src/lambda-calculus-library/lambda-calculus.js
@@ -1,36 +1,4 @@
-export {
-    I as id,
-    M,
-    K,
-    KI,
-    C,
-    B,
-    T,
-    V,
-    Blackbird,
-    fst,
-    snd,
-    firstOfTriple,
-    secondOfTriple,
-    thirdOfTriple,
-    True,
-    False,
-    If,
-    Then,
-    Else,
-    not,
-    and,
-    or,
-    beq,
-    showBoolean,
-    jsBool,
-    pair,
-    triple,
-    mapPair,
-    showPair,
-    churchBool,
-    LazyIf
-}
+export {I as id, M, K, KI, C, B, T, V, Blackbird, fst, snd, firstOfTriple, secondOfTriple, thirdOfTriple, True, False, If, Then, Else, not, and, or, beq, showBoolean, jsBool, pair, triple, mapPair, showPair, churchBool, LazyIf}
 
 /*
 idea shortcuts:
@@ -51,9 +19,9 @@ Ctrl-Shift-P  : expression type
 
 /**
  * x -> x ; Identity (id)
+ *
  * @lambda λx.x
  * @haskell Identity :: a -> a
- *
  * @function Identity
  * @param   {*} x
  * @returns {*} x
@@ -62,23 +30,23 @@ const I = x => x;
 
 /**
  * a -> b -> a ; Kestrel (Constant)
+ *
  * @lambda  λx.y.x
  * @haskell Kestrel :: a -> b -> a
- *
  * @function Konstant
  * @param    {*} x
- * @returns  { function(y:*): function(x:*) } a function that ignores its argument and returns x
+ * @returns  {function(y:*): function(x:*)} a function that ignores its argument and returns x
  */
 const K = x => y => x;
 
 /**
  * x -> y -> y ; Kite
+ *
  * @lambda  λx.y.y
  * @haskell Kite :: a -> b -> b
- *
  * @function Kite
  * @param    {*} x
- * @returns  { function(y:*): function(y:*) } a function that returns its argument y
+ * @returns  {function(y:*): function(y:*)} a function that returns its argument y
  */
 const KI = x => y => y;
 
@@ -86,23 +54,21 @@ const KI = x => y => y;
  * f -> f( f ) ; Mockingbird
  * @lambda  λf.ff
  * @haskell Mockingbird :: f -> f
- *
  * @function Mockingbird
  * @param   {function} f
- * @returns { function({ f: { f } }) } a self-application combinator
+ * @returns {function({ f: { f } })} a self-application combinator
  */
 const M = f => f(f);
 
 /**
  * f -> x -> y -> f( x )( y ) ; Cardinal (flip)
+ *
  * @lambda  λfxy.fxy
  * @haskell Cardinal :: f -> a -> b -> pair
- *
  * @function Cardinal
  * @function flip
  * @param    {function} f
  * @returns  { function(x:*): function(y:*): {f: { y x }} } The Cardinal, aka flip, takes two-argument function, and produces a function with reversed argument order.
- *
  * @example
  * C(K) (1)(2)  === 2
  * C(KI)(1)(21) === 1
@@ -115,11 +81,9 @@ const C = f => x => y => f(y)(x);
  *
  * @lambda λfgx.f(gx)
  * @haskell Bluebird :: f -> a -> b -> c
- *
  * @function Bluebird
  * @param   {function} f
  * @returns { function(g:function): function(x:*):  {f: { g:{x} } } } two-fold self-application composition
- *
  * @example
  * B(id)(id)(n7)     === n7
  * B(id)(jsnum)(n7)  === 7
@@ -133,10 +97,9 @@ const B = f => g => x => f(g(x));
  *
  * @lambda  λxf.fx
  * @haskell Thrush :: a -> f -> b
- *
  * @function Thrush
  * @param    {*} x
- * @returns  { function(f:function): {f: {x} } } self-application with holden argument
+ * @returns  {function(f:function): {f: {x} }} self-application with holden argument
  */
 const T = x => f => f(x);
 
@@ -145,11 +108,10 @@ const T = x => f => f(x);
  * x -> y -> f -> f (x)(y) ; Vireo (hold pair of args)
  *
  * @lambda  λxyf.fxy
- * @haskell Vireo :: a -> b -> f
- *
+ * @haskell Vireo :: a -> b -> f -> c
  * @function Vireo
  * @param    {*} x
- * @returns  { function(y:*): function(f:function): {f: {x y} } }
+ * @returns  {function(y:*): function(f:function): {f: {x y} }}
  */
 const V = x => y => f => f(x)(y);
 
@@ -159,10 +121,9 @@ const V = x => y => f => f(x)(y);
  *
  * @lambda  λfgxy.f(gxy)
  * @haskell Blackbird :: f -> g -> a -> b -> c
- *
  * @function
  * @param   {function} f
- * @returns { function(g:function): function(x:*): function(y:*): function({ f:{g: {x y}} }) }
+ * @returns {function(g:function): function(x:*): function(y:*): function({ f:{g: {x y}} })}
  * @example
  * Blackbird(x => x)    (x => y => x + y)(2)(3) === 5
  * Blackbird(x => x * 2)(x => y => x + y)(2)(3) === 10
@@ -171,6 +132,7 @@ const Blackbird = f => g => x => y => f(g(x)(y));
 
 /**
  * x -> y -> y ; {churchBoolean} False Church-Boolean
+ *
  * @function
  * @type    churchBoolean
  * @return  KI
@@ -179,12 +141,12 @@ const False = KI;
 
 /**
  * x -> y -> x ; {churchBoolean} True Church-Boolean
+ *
  * @function
  * @type    churchBoolean
  * @return  K
  */
 const True = K;
-
 
 /**
  * Syntactic sugar for creating a If-Then-Else-Construct
@@ -195,10 +157,10 @@ const True = K;
  * @constructor
  * @example
  * If( eq(n1)(n1) )
- *    (Then( "same"     ))
- *    (Else( "not same" ))
+ *  (Then( "same"     ))
+ *  (Else( "not same" ))
  */
-const If     = condition => truthy => falsy =>  condition(truthy)(falsy);
+const If = condition => truthy => falsy =>  condition(truthy)(falsy);
 
 /**
  * Syntactic sugar for creating a If-Then-Else-Construct for lazy Evaluation - it avoid that JavaScript eagerly evaluate both cases (then and else)
@@ -209,8 +171,8 @@ const If     = condition => truthy => falsy =>  condition(truthy)(falsy);
  * @constructor
  * @example
  * LazyIf( eq(n1)(n1) )
- *      (Then( () => "same"     ))
- *      (Else( () => "not same" ))
+ *  (Then( () => "same"     ))
+ *  (Else( () => "not same" ))
  */
 const LazyIf = condition => truthy => falsy => ( condition(truthy)(falsy) )();
 
@@ -238,7 +200,7 @@ const not = C;
  *
  * @function
  * @param   {churchBoolean} p
- * @returns { function(q:churchBoolean): churchBoolean }  True or False
+ * @returns {function(q:churchBoolean): churchBoolean}  True or False
  */
 const and = p => q => p(q)(False);
 
@@ -247,7 +209,7 @@ const and = p => q => p(q)(False);
  *
  * @function
  * @param   {churchBoolean} p
- * @returns { function(q:churchBoolean): churchBoolean }  True or False
+ * @returns {function(q:churchBoolean): churchBoolean}  True or False
  */
 const or = p => q => p(True)(q);
 
@@ -256,7 +218,7 @@ const or = p => q => p(True)(q);
  *
  * @function
  * @param   {churchBoolean} p
- * @returns { function( q:churchBoolean): churchBoolean }  True or False
+ * @returns {function( q:churchBoolean): churchBoolean}  True or False
  * @example
  * beq(True)(True)   === True;
  * beq(True)(False)  === False;
@@ -266,8 +228,9 @@ const beq = p => q => p(q)(not(q));
 
 /**
  * b -> b("True")("False") ; showBoolean
+ *
  * @function
- * @param b {churchBoolean}
+ * @param  {churchBoolean} b
  * @return {string} - "True" or "False"
  * @example
  * showBoolean(True)  === "True";
@@ -304,7 +267,7 @@ const churchBool = b => b ? True : False;
  *
  * @function
  * @param   {*} x:  firstOfPair argument of the pair
- * @returns { function(y:*): function(f:function): {f: {x y} } } - returns a function, that store two value
+ * @returns {function(y:*): function(f:function): {f: {x y} }} - returns a function, that store two value
  */
 const pair = V;
 
@@ -328,13 +291,12 @@ const fst = K;
  */
 const snd = KI;
 
-
 /**
- *  x -> y -> z -> f -> f(x)(y)(z) ; Triple
+ * x -> y -> z -> f -> f(x)(y)(z) ; Triple
  *
  * @function
- * @param {*} x - firstOfTriple argument of the Triple
- * @return { function(y:*):  function(z:*): function(f:function): {f: {x y z}} } - returns a function, that storage three arguments
+ * @param  {*} x - firstOfTriple argument of the Triple
+ * @return {function(y:*):  function(z:*): function(f:function): {f: {x y z}}} - returns a function, that storage three arguments
  */
 const triple = x => y => z => f => f(x)(y)(z);
 
@@ -344,8 +306,8 @@ const triple = x => y => z => f => f(x)(y)(z);
  * x -> y -> z -> x ; firstOfTriple
  *
  * @function
- * @param {*} x
- * @return { function(y:*): function(z:*): x } - x
+ * @param  {*} x
+ * @return {function(y:*): function(z:*): x} - x
  * @example
  * triple(1)(2)(3)(firstOfTriple) === 1
  */
@@ -355,10 +317,10 @@ const firstOfTriple = x => y => z => x;
  * x -> y -> z -> y ; secondOfTriple
  *
  * @function
- * @param {a} x
- * @return { function(y:*): function(z:*): y } - y
+ * @param  {*} x
+ * @return {function(y:*): function(z:*): y} - y
  * @example
- * triple(1)(2)(3)(firstOfTriple) === 2
+ * triple(1)(2)(3)(secondOfTriple) === 2
  */
 const secondOfTriple = x => y => z => y;
 
@@ -367,9 +329,9 @@ const secondOfTriple = x => y => z => y;
  *
  * @function
  * @param {*} x
- * @return { function(y:*): function(z:*): z } - z
+ * @return {function(y:*): function(z:*): z} - z
  * @example
- * triple(1)(2)(3)(firstOfTriple) === 3
+ * triple(1)(2)(3)(thirdOfTriple) === 3
  */
 const thirdOfTriple = x => y => z => z;
 
@@ -377,7 +339,7 @@ const thirdOfTriple = x => y => z => z;
  * function -> pair -> pair
  *
  * @function
- * @param {function} f
+ * @param  {function} f
  * @return {function(pair): function(Function): {f: {x, y}}} new mapped pair
  * @example
  * mapPair( x => x+3 )( pair(1)(2) ) === pair(4)(5)
@@ -389,7 +351,7 @@ const mapPair = f => p => pair(f(p(fst)))(f(p(snd)));
  * p -> p `${p(fst)} | ${p(snd)} ; showPair
  *
  * @function
- * @param {pair} p
+ * @param  {pair} p
  * @return string with first and second value
  * @example
  * const testPair = pair('Erster')('Zweiter');
